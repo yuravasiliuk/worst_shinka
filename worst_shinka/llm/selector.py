28b3e0badbcba59372ded39b0145c03736d1219c
Mykola Chernenko <mykolachernenko2004@gmail.com>
2026-08-26 11:20:19 +0200

    updated verification of updated probabilities for llm models, added description of methods in the model selector script

--- worst_shinka/llm/selector.py
+++ worst_shinka/llm/selector.py
@@ -2,31 +2,50 @@ from .pool_reader import Reader_Model_Pool
 import numpy as np
 import yaml
 
+models_probabilities = {}
+
 class Selector_LLM():
-    def __init__(self, model_ids, low_threshold=0.1, upper_threshold=0.9, delta_probability=0.05):
+    def __init__(self, model_ids, lower_threshold=0.1, upper_threshold=0.9, delta_probability=0.05):
+        """
+        Initialization of probability distribution of models. 
+        At the beginning the distribution is uniform.
+        """
         self.model_ids = model_ids 
         self.probabilities = [1/len(model_ids) for _ in range(len(model_ids))]
-        self.low_threshold = low_threshold
+        self.lower_threshold = lower_threshold
         self.upper_threshold = upper_threshold
         self.delta_probability = delta_probability
 
     def select_models(self):
+        """
+        Method selects two models based on corresponding probability distribution.
+        Method return two model ids.
+        """
         two_selected_model_ids = np.random.choice(self.model_ids, size = 2, replace = False, p = self.probabilities)
         return two_selected_model_ids
 
-    def check_prob_in_bounds(self, model_id_index):
-        return self.probabilities[model_id_index] < self.upper_threshold and self.probabilities[model_id_index] > self.low_threshold
-
     def update_probabilities(self, model_id_1, model_id_2, who_won): 
+        """
+        Method updates probabilities of two arguing models. 
+        The winner gets a higher probability of being selected for the next discussion, symmetrically the loser obtains a lower probability.
+        The change in probabilities is constant == self.delta_probability.
+        Change in probabilities is possible if and only if new probabilities are within the preset lower and upper bound.
+        It is assumed that the model with id model_id_1 has won if who_won == 1, otherwise it is assumed that the second model has won.
+        """
         index_1 = self.model_ids.index(model_id_1)
         index_2 = self.model_ids.index(model_id_2)
-        if self.check_prob_in_bounds(index_1) and self.check_prob_in_bounds(index_2):
-            if who_won == 1:
-                self.probabilities[index_1] += self.delta_probability
-                self.probabilities[index_2] -= self.delta_probability
-            else:
-                self.probabilities[index_2] += self.delta_probability
-                self.probabilities[index_1] -= self.delta_probability
+        if who_won == 1:
+            new_prob_1 = self.probabilities[index_1] + self.delta_probability
+            new_prob_2 = self.probabilities[index_2] - self.delta_probability
+            if (new_prob_1 < self.upper_threshold) and (new_prob_2 > self.lower_threshold):
+                self.probabilities[index_1] = new_prob_1
+                self.probabilities[index_2] = new_prob_2
+        else:
+            new_prob_1 = self.probabilities[index_1] - self.delta_probability
+            new_prob_2 = self.probabilities[index_2] + self.delta_probability
+            if (new_prob_1 > self.lower_threshold) and (new_prob_2 < self.upper_threshold):
+                self.probabilities[index_1] = new_prob_1
+                self.probabilities[index_2] = new_prob_2
 
 reader = Reader_Model_Pool()
 reader.read_model_ids_from_yaml("Test")
